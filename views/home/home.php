7f6d3e2206af5d020ac460769b5b3323216aa162
tuandoan <doan.manhtuan511@gmail.com>
2020-04-22 20:50:08 +0700

    test

--- views/home/home.php
+++ views/home/home.php
@@ -6,16 +6,15 @@
     <title>HOME</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/3e4a6334d7.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/Final/decor/home_style.css">
+    <link rel="stylesheet" href="../../decor/home_style.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script>
+    <script type="text/javascript">
         $(document).ready(function () {
             $(".hamburger").click(function () {
                 $(".wrapper").toggleClass("collapse")
             });
         });
-    </script>
-    <script type="text/javascript">
+
         $(document).ready(function () {
             $("#user").mouseover(function () {
                 $(".user_c").show(100);
@@ -24,6 +23,22 @@
                 $(".user_c").hide(100);
             });
         });
+
+        $(document).ready(function () {
+            $(".detail").click(function () {
+                $(".wrapper").toggleClass("collapse_detail")
+            });
+        });
+
+        $(document).ready(function () {
+            $("#show").mouseover(function () {
+                $(".sub-menu").show(100);
+            });
+            $("html").click(function () {
+                $(".sub-menu").hide(100);
+            });
+        });
+
     </script>
 
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
@@ -51,18 +66,18 @@
                     <div class="head">
                         <div class="user_section" id="user">
                             <p class="v">Nguyen Phuong</p>
-                            <img src="/Final/image/avatar.svg" alt="user" class="l">
+                            <img src="../../image/avatar.svg" alt="user" class="l">
                         </div>
                         <div class="user_c">
                             <div class="user_info l">
-                            <img src="/Final/image/avatar.svg" alt="user" class="info_img ">
-                            <p >Username</p>
-                            <p>User@gmail.com</p>
+                                <img src="../../image/avatar.svg" alt="user" class="info_img ">
+                                <p>Username</p>
+                                <p>User@gmail.com</p>
+                            </div>
 
-                        </div>
                                 <ul>
                                     <li><a href="#">Manage your Profile</a></li>
-                                    <li><a href="?controller=logout&action=logout">Logout</a></li>
+                                    <li><a href="#">Logout</a></li>
                                 </ul>
 
                         </div>
@@ -72,6 +87,28 @@
             </div>
             <div class="sidebar">
                 <ul>
+                    <li id="show"><a href="#">
+                            <span class="icon" id="show">
+                                <i class="fas fa-plus-circle" aria-hidden="true"></i></span>
+                            <span class="title">New</span>
+                        </a>
+                        <div class="sub-menu">
+                            <ul>
+                                <li><a href="#">
+                                         <span class="icon" >
+                                        <i class="fas fa-folder-plus" aria-hidden="true"></i></span>
+                                        <span class="Add">Create Folder</span></a></li>
+                                <li><a href="#">
+                                         <span class="icon" >
+                                        <i class="fas fa-file-upload" aria-hidden="true"></i></span>
+                                        <span class="Add">Upload File</span></a></li>
+                                <li><a href="#">
+                                         <span class="icon">
+                                        <i class="fas fa-folder" aria-hidden="true"></i></span>
+                                        <span class="Add">Upload Folder</span></a></li>
+                            </ul>
+                        </div>
+                    </li>
                     <li><a href="#">
                             <span class="icon">
                                 <i class="fas fa-box-open" aria-hidden="true"></i></span>
@@ -102,58 +139,28 @@
                                 <i class="fas fa-cog" aria-hidden="true"></i></span>
                             <span class="title">Setting</span>
                         </a></li>
+
                 </ul>
             </div>
+            <div class="detail">
+                <a href="#">
+                            <span class="icon_detail">
+                                <i class="fas fa-info-circle" aria-hidden="true"></i></span>
 
-            <div class="main_container">
-                <input class="file-input" type="file" name="myFile[]" id="myFile" multiple  />
-                <button class="file_button" type="button" >Upload File</button>
-                <?php
-                    echo "<a href='?controller=home&action=index'>My Drive</a>";
-                    $url = "";
+                    </a>
+            </div>
+            <div class="detailbar">
+                <nav>
+                    <a href="#">Details</a>
+                    <a href="#">Activity</a>
+                    <div class="line"></div>
+                 </nav>
+            </div>
 
-                    foreach (explode("/", $dir) as $item) {
-                        if ($item == "") {
-                            continue;
-                        }
-                        $url = $url . "/" . $item;
-                        if ($url == "") {
-                            $url = $item;
-                        }
-                        echo "  >  <a href='?controller=home&action=index&dir=$url'>$item</a>";
-                    }
 
-                    echo "<br>";
-
-                    $files = scandir($dir_path);
-                    foreach ($files as $file) {
-                        if (substr($file, 0, 1) == '.'){
-                            continue;
-                        }
-                        if (is_dir($dir_path . "/" . $file)) {
-                            $file_path = str_replace($root . "/", "", $dir_path . "/" . $file);
-                            echo "<a href='?controller=home&action=index&dir=$file_path'>$file<a>";
-                            echo "<br>";
-                        }
-                        else
-                        {
-                            $file_path = str_replace($root . "/", "", $dir_path . "/" . $file);
-                            echo "<a href='?controller=home&action=index&file=$file_path'>$file<a>";
-                            echo "<br>";
-                        }
-                    }
-                ?>
-                <form action="/Final/" method="get">
-                    <input type="hidden" name="controller" value="home"/>
-                    <input type="hidden" name="action" value="create_dir"/>
-                    <input type="hidden" name="dir" value="<?= substr($dir, 1) ?>"/>
-                    <input type="text" name="new_folder_name"/>
-                    <input type="submit" value="create new folder"/>
-                </form>
-                <script type="text/javascript" src="/Final/decor/home.js"></script>
 
         </div>
-        </div>
+
     </header>
 
 </body>
